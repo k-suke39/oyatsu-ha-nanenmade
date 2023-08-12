737da94e7462828bced3685d2dea93351893b213
k-suke39 <pz.mozarla@gmail.com>
2023-08-13 07:56:20 +0900

    ⚡️

--- app/page.tsx
+++ app/page.tsx
@@ -236,7 +236,7 @@ export default function Page() {
 					</h5>
 					<input
 						type="number"
-						value={budget}
+						value={budget !== null ? budget.toString() : ''}
 						onChange={handleBudgetChange}
 						className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
 						placeholder="おこづかいを100~300円で入れてね"

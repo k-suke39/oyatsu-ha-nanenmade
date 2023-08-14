ff186e781f409483e5a3c55f12acdb5a39bb28ad
k-suke39 <pz.mozarla@gmail.com>
2023-08-14 13:42:16 +0900

    ⚡️

--- app/page.tsx
+++ app/page.tsx
@@ -238,10 +238,7 @@ export default function Page() {
 	return (
 		<div>
 			<div className="flex justify-center items-center flex-col h-screen">
-				<a
-					href="#"
-					className="block p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700"
-				>
+				<div className="block p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
 					<h5 className="mb-5 text-2xl font-bold  text-gray-900 dark:text-white">
 						今日のおやつは何円まで？
 					</h5>
@@ -252,8 +249,8 @@ export default function Page() {
 						className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
 						placeholder="おこづかいを100~300円で入れてね"
 						required
-					></input>
-				</a>
+					/>
+				</div>
 				<div className="flex justify-center items-center flex-col">
 					{overBudget && (
 						<h5 className="mt-4 mb-2 text-7xl font-bold tracking-tight text-red-500 dark:text-white">
